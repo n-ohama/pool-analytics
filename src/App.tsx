67a27ccebf4eacb9751631bdb46d3f39491da68c
enu <enu1996502@gmail.com>
2022-09-17 21:40:46 +0900

    react version 18 完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { Home } from "./pages/home";
 
-function App() {
+const links = [{ path: "/", element: <Home /> }];
+
+const Router = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {links.map((item) => {
+        return (
+          <Route key={item.path} path={item.path} element={item.element} />
+        );
+      })}
+    </Routes>
   );
-}
+};
 
-export default App;
+export const App = () => (
+  <ChakraProvider>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </RecoilRoot>
+  </ChakraProvider>
+);
